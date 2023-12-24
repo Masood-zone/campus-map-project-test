2e248bee8d417e49c985ef30d6d05ecd7dec4abb
Masood <masoodacheampong@gmail.com>
2023-12-24 20:38:02 +0000

    Added a tenery operation to check whether something has been stored in the location state.

--- src/pages/location/LocationList.js
+++ src/pages/location/LocationList.js
@@ -6,31 +6,38 @@ function LocationList({ data, onDelete, onEdit }) {
   return (
     <section>
       <div>
-        {data.map((location) => (
-          <div
-            key={location.id}
-            className="border-2 border-gray-100 my-3 mx-4 p-2 flex items-start rounded-md justify-between gap-8"
-          >
-            <div>
-              <h1 className="font-medium">{location.name}</h1>
-              <p>{location.description}</p>
+        {data.length > 0 ? (
+          data.map((location) => (
+            <div
+              key={location.id}
+              className="border-2 border-gray-100 my-3 mx-4 p-2 flex items-start rounded-md justify-between gap-8"
+            >
+              <div>
+                <h1 className="font-medium">{location.name}</h1>
+                <p>{location.description}</p>
+              </div>
+              <div className="flex items-center gap-4">
+                <button
+                  className="btn btn-circle"
+                  onClick={() => onDelete(location.id)}
+                >
+                  <TiDelete fontSize={20} />
+                </button>
+                <button
+                  className="btn btn-circle"
+                  onClick={() => onEdit(location)}
+                >
+                  <FaEdit fontSize={20} />
+                </button>
+              </div>
             </div>
-            <div className="flex items-center gap-4">
-              <button
-                className="btn btn-circle"
-                onClick={() => onDelete(location.id)}
-              >
-                <TiDelete fontSize={20} />
-              </button>
-              <button
-                className="btn btn-circle"
-                onClick={() => onEdit(location)}
-              >
-                <FaEdit fontSize={20} />
-              </button>
-            </div>
+          ))
+        ) : (
+          <div className="text-left my-4 mx-4">
+            <p>It seems you donot have any location saved.</p>
+            <p>Be sure to add some.</p>
           </div>
-        ))}
+        )}
       </div>
     </section>
   );
